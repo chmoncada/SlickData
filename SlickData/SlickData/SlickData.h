b48fdf88789f8f7b96c310e27313caa50a280560
Fernando Rodriguez <frr@agbo.biz>
2015-11-29 15:34:54 +0100

    initial commit

---
+++ SlickData/SlickData/SlickData.h
@@ -0,0 +1,19 @@
+//
+//  SlickData.h
+//  SlickData
+//
+//  Created by Fernando Rodríguez Romero on 29/11/15.
+//  Copyright © 2015 Udacity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SlickData.
+FOUNDATION_EXPORT double SlickDataVersionNumber;
+
+//! Project version string for SlickData.
+FOUNDATION_EXPORT const unsigned char SlickDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SlickData/PublicHeader.h>
+
+
